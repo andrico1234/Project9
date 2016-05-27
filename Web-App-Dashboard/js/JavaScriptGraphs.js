b20e9f041a39e22f70a03ee9afb4abd4614dee64
andrico1234 <andrico1234@yahoo.co.uk>
2016-05-27 13:02:40 +0100

    v0.4
    Improved Layout
    Added first graph

--- Web-App-Dashboard/js/JavaScriptGraphs.js
+++ Web-App-Dashboard/js/JavaScriptGraphs.js
@@ -3,8 +3,13 @@
  */
 
 
+Chart.defaults.global.defaultFontFamily = "'Futura', 'Helvetica', 'Arial', sans-serif";
+Chart.defaults.global.defaultFontColor = "#bcbcbc";
+Chart.defaults.global.legend = false;
+
 var lineChart = document.getElementById("line-chart");
 var myChart = new Chart(lineChart, {
+
     type: 'line',
     data: {
         labels: ["16-22", "23-29", "30-5", "6-12", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
@@ -15,7 +20,7 @@ var myChart = new Chart(lineChart, {
             borderColor: "#7479BD",
             label: 'Traffic',
             pointBorderColor: '#4D4D71',
-            pointBackgroundColor: '#abaed7',
+            pointBackgroundColor: '#f1f1f8',
             pointBorderWidth: 2,
             pointRadius: 6,
             pointHoverRadius: 8,
